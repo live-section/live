e4d17828c868caf6015d7270e4481024a7d5b42b
Dean Kevorkian <deankevor@gmail.com>
2019-08-06 22:28:26 +0300

    Added NewPostFragment for new posts, with upload picture option. Added new navigation graph for the new live user activity.

---
+++ app/src/main/java/com/example/live/NewPostFragment.java
@@ -0,0 +1,67 @@
+package com.example.live;
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewPostFragment extends Fragment {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    final static int RESAULT_SUCCESS = 0;
+
+    ImageView selectedPictureImgView;
+
+    public NewPostFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            selectedPictureImgView.setImageBitmap(imageBitmap);
+            selectedPictureImgView.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View fragmentView = inflater.inflate(R.layout.fragment_new_post, container, false);
+
+        Button uploadPhotoButton = fragmentView.findViewById(R.id.newPostUploadPhotoButton);
+        this.selectedPictureImgView = fragmentView.findViewById(R.id.selectedPictureImgView);
+
+        uploadPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+        return fragmentView;
+    }
+
+}
